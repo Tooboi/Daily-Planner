4e82633ef3434188825a9a650af09c5f978bf105
Tooboi <japica123@gmail.com>
2022-11-17 00:56:19 -0500

    user can click button to save data but not yet get

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -36,50 +36,21 @@ $(function () {
     $('#hour-16'),
     $('#hour-17')
   ]
-
+  var saveItems = [];
   var dateEl = $('#currentDay');
   var now = dayjs().format('dddd, MMM D YYYY, h:mm A');
-  
-  
-  
-
-
-  
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
-  // Delegate event listener to the parent element, <div id="buttons">
- buttonListEl.on('click', '.letter-button', function (event) {
-   var displayLetterEl = $('<div>');
-   displayLetterEl.addClass('letter');
-   // get letter from clicked letter button's `data-letter` attribute and use it for display
-   displayLetterEl.text($(event.target).attr('data-letter'));
-   displayEl.append(displayLetterEl);
- });
-
-
 
   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // block by comparing the id to the current hour.
   // dayjs().hour()
-  var currentHour = 13;
+  var currentHour = 10;
   currentHour-=9;
-  console.log("current hour ", currentHour);
+  console.log("currentHour ", currentHour);
 
   for (let i = 0; i < hoursAr.length; i++) {
     var presentHour = i;
     var currentHourEl = hoursAr[i];
     
-
     if (presentHour === currentHour) {
       currentHourEl.attr('class', 'row time-block present')
      } 
@@ -89,11 +60,22 @@ $(function () {
      if (presentHour > currentHour) {
       currentHourEl.attr('class', 'row time-block future')
      }
-
 }
-  
-    console.log("present hour ", presentHour);
-    console.log("current element ", currentHourEl);
+
+  // TODO: Add a listener for click events on the save button. 
+  function clickButton(event) {
+    event.preventDefault;
+    var selectedButton = this;
+    var buttonParent = $(selectedButton).parent();
+    var buttonTextarea = $(selectedButton).siblings()[1].value;
+    var buttonName = buttonParent[0].id;
+
+    localStorage.setItem(JSON.stringify(buttonName), JSON.stringify(buttonTextarea))
+    console.log($(selectedButton).siblings()[1]);
+    
+  }
+  $('.btn').on('click', this, clickButton)
+
 
 
   // TODO: Add code to get any user input that was saved in localStorage and set
